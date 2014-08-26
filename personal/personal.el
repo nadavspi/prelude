7b2c79ae94b5c98c784432562dcf737b1ad32e53
Nadav Spiegelman <me@nadav.name>
2014-08-25 22:46:13 -0400

    Add personal config and relative line numbers

---
+++ personal/personal.el
@@ -0,0 +1,45 @@
+;;; personal.el --- Nadav's personal configuration
+
+;;; Commentary:
+;; Random configuration
+
+;;; Code:
+
+;; Disable flyspell
+(setq prelude-flyspell nil)
+
+;; Default theme
+(disable-theme 'zenburn)
+(load-theme 'color-theme-sanityinc-tomorrow-eighties)
+
+;; Theme functions
+(defun light ()
+(interactive)
+(color-theme-sanityinc-solarized-light))
+(defun dark ()
+(interactive)
+(color-theme-sanityinc-tomorrow-eighties))
+
+;; Font settings
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :height 180)
+  (set-default-font "input sans")
+  (toggle-word-wrap t)
+
+  (defun use-proportional-font ()
+    (interactive)
+    (face-remap-add-relative 'default '(:family "Input Sans"))
+  )
+
+  (defun use-monospace-font ()
+    (interactive)
+    (face-remap-add-relative 'default '(:family "Input Mono"))
+  ))
+
+(add-hook 'dired-mode-hook 'use-monospace-font)
+
+;; Line numbers
+(global-linum-mode t)
+
+(provide 'personal)
+;;; personal.el ends here
---
+++ personal/relativeline.el
@@ -0,0 +1,32 @@
+;;; relativeline.el --- Relative line numbers
+
+;;; Commentary:
+;; Relative line numbers like in Vim
+;; From here: https://github.com/xiaohanyu/oh-my-emacs/issues/50
+
+;;; Code:
+
+(defvar my-linum-format-string "%3d")
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+(defun my-linum-get-format-string ()
+  (let* ((width (1+ (length (number-to-string
+                             (count-lines (point-min) (point-max))))))
+         (format (concat "%" (number-to-string width) "d")))
+    (setq my-linum-format-string format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-relative-line-numbers)
+
+(defun my-linum-relative-line-numbers (line-number)
+  (let ((offset (abs(- line-number my-linum-current-line-number))))
+    (propertize (format my-linum-format-string offset) 'face 'linum)))
+
+(defadvice linum-update (around my-linum-update)
+  (let ((my-linum-current-line-number (line-number-at-pos)))
+    ad-do-it))
+(ad-activate 'linum-update)
+
+(provide 'relativeline)
+;;; relativeline.el ends here
